a2849b76732c29a7f08531423ee6a2a2e5324b24
jonata azevedo <manojhonson181@gmail.com>
2022-08-15 19:50:26 -0300

    first commit

---
+++ src/main/java/ubivis/teste/paradademaquinas/ParadaDeMaquinasApplication.java
@@ -0,0 +1,13 @@
+package ubivis.teste.paradademaquinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadaDeMaquinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadaDeMaquinasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ubivis/teste/paradademaquinas/ParadaDeMaquinasApplicationTests.java
@@ -0,0 +1,13 @@
+package ubivis.teste.paradademaquinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadaDeMaquinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
